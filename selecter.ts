f00990f32199586f8f78c83825f92d32bea00cda
DavidYo <lturtsamuel@gmail.com>
2019-03-24 13:20:49 +0800

    改變選擇器，使之可以分批吐出許多組參數

--- selecter.ts
+++ selecter.ts
@@ -7,21 +7,33 @@ class UISelecter {
 
 class Selecter {
     constructor(private card_table: { [index: number]: ICard }) { }
-    private selected_seqs = new Array<number>();
+    private selected_seqs = new Array<Array<number>>();
     private top = 0;
     public setSelectedSeqs(seqs: Array<number> | number) {
-        if(seqs instanceof Array) {
-            this.selected_seqs = seqs;
-        } else {
-            this.selected_seqs = [seqs];
-        }
         this.top = 0;
+        this.selected_seqs = [];
+        this.appendSelectedSeqs(seqs);
     }
+    public appendSelectedSeqs(seqs: Array<number> | number) {
+        if(seqs instanceof Array) {
+            this.selected_seqs.push(seqs);
+        } else {
+            this.selected_seqs.push([seqs]);
+        }
+    }
+
     private select(card_type: CardType, max: number , min: number,
         checkCanSelect=(char: ICard) => true
     ): ICard[] {
-        let seqs = this.selected_seqs.slice(this.top, this.top+max);
-        let chars = seqs.map(seq => {
+        if(this.selected_seqs.length <= this.top) {
+            throw new BadOperationError(`選到的數量不在${min}~${max}的範圍`);
+        }
+        let seqs = this.selected_seqs[this.top++];
+        let len = seqs.length;
+        if(len > max || len < min) {
+            throw new BadOperationError(`選到的數量不在${min}~${max}的範圍`);
+        }
+        let cards = seqs.map(seq => {
             let card = this.card_table[seq];
             if(card.card_type == card_type) {
                 if(checkCanSelect(card)) {
@@ -34,12 +46,8 @@ class Selecter {
                     `欲選擇${CardType[card_type]}，結果選到${CardType[card.card_type]}！`);
             }
         });
-        let len = chars.length;
-        if(len > max || len < min) {
-            throw new BadOperationError(`選到的數量不在${min}~${max}的範圍`);
-        }
         this.top += len;
-        return chars;
+        return cards;
     }
     public selectChars(max=1, min=1,
         checkCanSelect=(char: ICharacter) => true
